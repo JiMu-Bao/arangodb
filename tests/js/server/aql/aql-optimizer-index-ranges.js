2e6b66beeaa434f19bd8d96b08d7742bef4e93c4
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-01-07 17:55:44 +0100

    add test for _id

--- tests/js/server/aql/aql-optimizer-index-ranges.js
+++ tests/js/server/aql/aql-optimizer-index-ranges.js
@@ -134,6 +134,31 @@ function optimizerIndexesRangesTestSuite () {
         ],[
           "FOR i IN " + c.name() + " FILTER ('test1997' <= i._key) RETURN i._key",
           [ "test1997", "test1998", "test1999" ]
+        ],[
+
+          "FOR i IN " + c.name() + " FILTER (i._id < '" + c.name() + "/test0002') RETURN i._key",
+          [ "test0000", "test0001" ]
+        ],[
+          "FOR i IN " + c.name() + " FILTER ('" + c.name() + "/test0002' > i._id) RETURN i._key",
+          [ "test0000", "test0001" ]
+        ],[
+          "FOR i IN " + c.name() + " FILTER (i._id > '" + c.name() + "/test1997') RETURN i._key",
+          [ "test1998", "test1999" ]
+        ],[
+          "FOR i IN " + c.name() + " FILTER ('" + c.name() + "/test1997' < i._id) RETURN i._key",
+          [ "test1998", "test1999" ]
+        ],[
+          "FOR i IN " + c.name() + " FILTER (i._id <= '" + c.name() + "/test0002') RETURN i._key",
+          [ "test0000", "test0001", "test0002" ]
+        ],[
+          "FOR i IN " + c.name() + " FILTER ('" + c.name() + "/test0002' >= i._id) RETURN i._key",
+          [ "test0000", "test0001", "test0002" ]
+        ],[
+          "FOR i IN " + c.name() + " FILTER (i._id >= '" + c.name() + "/test1997') RETURN i._key",
+          [ "test1997", "test1998", "test1999" ]
+        ],[
+          "FOR i IN " + c.name() + " FILTER ('" + c.name() + "/test1997' <= i._id) RETURN i._key",
+          [ "test1997", "test1998", "test1999" ]
         ]
       ];
 
